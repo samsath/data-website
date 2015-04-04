6985faf47bdc1526a01f1654a9993f1165fd1127
Eddy Lane <Naedin@gmail.com>
2015-04-04 15:39:53 +0100

    Removed dep on d3.promise because it seemed to be slowing things down slightly. Added docblocks.

--- js/map.js
+++ js/map.js
@@ -1,6 +1,11 @@
 ;
 (function (CONFIG, d3, Handlebars) {
 
+    /**
+     * Gets a slugified version of a string
+     * @param {string} text
+     * @returns {string}
+     */
     function slugify(text) {
         return text.toString().toLowerCase()
             .replace(/\s+/g, '-')           // Replace spaces with -
@@ -10,8 +15,16 @@
             .replace(/-+$/, '');            // Trim - from end of text
     }
 
+    /**
+     * Map Class
+     *
+     * Intantiate a new map and data-table instance by passing in a css selector for where to place the map, and a css selector for where to put table results
+     *
+     * @param {string} mapSelector          Css selector
+     * @param {string} tableSelector        Css selector
+     * @constructor
+     */
     function Map(mapSelector, tableSelector) {
-
         var parentElement = document.querySelector(mapSelector);
 
         var e = document.createElement('object');
@@ -19,35 +32,48 @@
         e.type = "image/svg+xml";
         e.width = 594;
         e.height = 806;
+
         e.addEventListener('load', _.bind(this.onSvgLoaded, this));
 
         this.element = e;
-        this.centered = null;
         this.tableSelector = tableSelector;
-
-        this.topoJson = d3.promise.json('/json/gb/topo_wpc.json');
-        this.apiJson = d3.promise.json(CONFIG.apiBaseUrl + '/constituencies/results/parties.json?filters=leading');
-
-        this.apiJson.then(_.bind(this.tabulate, this));
+        this.centered = null;
 
         parentElement.appendChild(e);
     }
 
+    /**
+     * When the SVG is finished loading hook up D3 to the element and load the tpo_wpc.json so we can add click listeners
+     * Requests the data from the API and inits the data-table and maps the results to the SVG
+     *
+     * @param {Object} e     The 'load' event
+     */
     Map.prototype.onSvgLoaded = function onSvgLoaded(e) {
 
-        var svgDocument = e.target.getSVGDocument().documentElement;
-
-        svgDocument.addEventListener('click', _.bind(function (e) {
-            if (e.target.nodeName === 'svg') {
-                this.toggleSelectedArea();
-            }
-        }, this));
-
-        // Select the map SVG element
-        var svg = d3.select(svgDocument);
+        // Create SVG element for the map
+        var svg = d3.select(e.target.getSVGDocument().documentElement)
+                .attr('width', this.element.width)
+                .attr('height', this.element.height)
+            ;
 
         this.g = svg.select('g');
 
+        d3.json(CONFIG.apiBaseUrl + '/constituencies/results/parties.json?filters=leading', _.bind(function (error, constituencies) {
+            this.tabulate(constituencies);
+            this.mapConstituencyResults(constituencies);
+        }, this));
+
+        d3.json('/json/gb/topo_wpc.json', _.bind(function (error, boundaries) {
+            this.addClickListeners(boundaries);
+        }, this));
+    };
+
+    /**
+     * From a given TopoJSON json file (https://github.com/mbostock/topojson) that matches the SVG that is saved
+     * Create click event listeners on constituency paths to scale/translate the map
+     * @param {Object} boundaries
+     */
+    Map.prototype.addClickListeners = function addClickListeners(boundaries) {
         // Set up projection
         this.projection = d3.geo.albers()
             .rotate([0, 0]);
@@ -59,12 +85,6 @@
             .scale(1)
             .translate([0, 0]);
 
-        this.topoJson.then(_.bind(this.addClickListeners, this));
-
-        this.addMapClasses();
-    };
-
-    Map.prototype.addClickListeners = function addClickListeners(boundaries) {
         // Compute correct bounds and scaling from the TopoJSON
         var b = this.path.bounds(topojson.feature(boundaries, boundaries.objects['wpc']));
         var s = .95 / Math.max((b[1][0] - b[0][0]) / this.element.width, (b[1][1] - b[0][1]) / this.element.height);
@@ -77,21 +97,26 @@
         this.g.selectAll('.area')
             .data(topojson.feature(boundaries, boundaries.objects['wpc']).features)
             .on('click', _.bind(this.toggleSelectedArea, this));
+
     };
 
-    // Select a map area
-    Map.prototype.toggleSelectedArea = function toggleSelectedArea(d) {
+    /**
+     * Scale and translate the map to a particular constituency, or zoom back out if already selected or nothing is passed in
+     * @param {Object} [constituencyPath]
+     */
+    Map.prototype.toggleSelectedArea = function toggleSelectedArea(constituencyPath) {
 
         // Handle map zooming
         var x, y, k;
         var changedArea = false;
+        var slugified;
 
-        if (d && this.centered !== d) {
-            var centroid = this.path.centroid(d);
+        if (constituencyPath && this.centered !== constituencyPath) {
+            var centroid = this.path.centroid(constituencyPath);
             x = centroid[0];
             y = centroid[1];
             k = 8;
-            this.centered = d;
+            this.centered = constituencyPath;
             changedArea = true;
 
         } else {
@@ -110,13 +135,13 @@
             });
 
         // Remove currently highlighted table row
-        d3.select(this.tableSelector + ' .selected')
+        d3.select('#data_table .selected')
             .classed({selected: false});
 
         if (this.centered && changedArea) {
             // Highlight corresponding table row
 
-            var slugified = slugify(d.properties.PCON13NM);
+            slugified = slugify(constituencyPath.properties.PCON13NM);
             //Slightly irritating exception due to accent.
             if (slugified === 'ynys-mon') {
                 slugified = 'ynys-mn';
@@ -135,34 +160,35 @@
             .style("stroke-width", 1 / k + "px");
     };
 
-    Map.prototype.tabulate = function tabulate(data) {
-
+    /**
+     * Using handlebars create DOM for the data-table from a given set of constituencies
+     * Add event listener on to the table that zooms to a particular constituency
+     * @param {Object[]} constituencies   An array of constituency data from the API
+     */
+    Map.prototype.tabulate = function tabulate(constituencies) {
         var table = document.querySelector(this.tableSelector);
-
         table.innerHTML = Handlebars.compile(document.querySelector('#constituency-list').innerHTML)({
-            constituencies: data
+            constituencies: constituencies
         });
-
         table.addEventListener('click', _.bind(function (e) {
             var mapElement = this.g.select('.' + e.target.parentNode.classList.item(0));
             this.toggleSelectedArea(mapElement.data()[0]);
         }, this));
-
     };
 
-    Map.prototype.addMapClasses = function loadData() {
+    /**
+     * Add classes on to the constituency paths in the SVG from a given set of constituency results
+     * @param {Object[]} constituencies   An array of constituency data from the API
+     */
+    Map.prototype.mapConstituencyResults = function mapConstituencyResults(constituencies) {
         var svgDoc = this.element.contentDocument || this.element.getSVGDocument();
-        this.apiJson.then(function (constituencies) {
-            _.forEach(constituencies, function (constituency) {
-                var constituencyNode = svgDoc.querySelector('.' + slugify(constituency.constituency_name));
-                constituencyNode.classList.add('party-' + constituency.party_name.toLowerCase().replace(/ /g, '-'));
-            });
+        _.forEach(constituencies, function (constituency) {
+            var constituencyNode = svgDoc.querySelector('.' + slugify(constituency.constituency_name));
+            constituencyNode.classList.add('party-' + constituency.party_name.toLowerCase().replace(/ /g, '-'));
         });
     };
 
-
     Handlebars.registerHelper('slugify', slugify);
     new Map('#map', '#constituency-rows');
-
 
 })(VFP_DATA_CONFIG, d3, Handlebars);
