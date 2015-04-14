7d09d55e487adbed432d307a336c5cd83ca15d10
Simon Plenderleith <simon@simonplend.co.uk>
2015-04-14 07:08:51 +0100

    First bash at restructuring site to use DocPad
    - Layout template uses ECO
    - Google Analytics tracking code now won't render in development
    - JavaScript files can be specified in the meta for each page (basic
      implementation)

---
+++ docpad.js
@@ -0,0 +1,10 @@
+// DocPad Configuration File
+// http://docpad.org/docs/config
+
+// Define the DocPad Configuration
+var docpadConfig = {
+	
+};
+
+// Export the DocPad Configuration
+module.exports = docpadConfig
